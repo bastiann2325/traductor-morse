e0a7b363957bfe67cc2ed71339c86c0e9bffc73e
Juan Sebastian Mejia Zuniga <juansebas7844@gmail.com>
2025-04-17 15:52:47 -0500

    feat: Agregar lógica para levantar servidor básico en main.go

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"net/http"
+	"log"
+	"html/template"
+)
+
+func main() {
+	// Cargar templates
+	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
+
+	// Ruta para servir el formulario
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, nil)
+	})
+
+	// Servir archivos estáticos (Css, Js, imágenes, etc.)
+	http.Handle("/web/static/",http.StripPrefix("/web/static", http.FileServer(http.Dir("web/static"))))
+
+	// Levantar servidor en el puerto 8080
+	log.Println("Servidor escuchando en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
