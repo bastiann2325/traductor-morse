daa058d0ac13525e5a85c33ee36fc1f0b4abd5b4
Juan Sebastian Mejia Zuniga <juansebas7844@gmail.com>
2025-04-17 21:07:44 -0500

    docs: Agregar comentarios al archivo main.go

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 )
 
 func main() {
-	// Cargar template principal
+	// Cargar archivo html como plantilla web
 	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
 
-	// Ruta principal (sirve el HTML)
+	// Ruta principal (sirve el HTML), muestra el contenido de index.html
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Escribe el html en la respuesta web
 		tmpl.Execute(w, nil)
 	})
 
@@ -21,24 +22,27 @@ func main() {
 	fs := http.FileServer(http.Dir("web/static"))
 	http.Handle("/web/static/", http.StripPrefix("/web/static/", fs))
 
-	// Endpoint para traducir
+	// Definir ruta de tipo API para recibir datos y responder con una traducción
 	http.HandleFunc("/traducir", traducirHandler)
 
-	// Iniciar servidor
+	// Iniciar servidor en el puerto 8080
 	log.Println("Servidor escuchando en http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
+	// Si occurre algún error al iniciar el servidor, se muestra aquí
 	if err != nil {
 		log.Fatal("Error iniciando servidor:", err)
 	}
 }
 
-// Handler para procesar traducciones
+// Handler para procesar traducciones, esta función se ejecuta cuando alguien hace una petición POST a /traducir
 func traducirHandler(w http.ResponseWriter, r *http.Request) {
+	// Verificar que se usa el método POST
 	if r.Method != http.MethodPost {
 		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
 		return
 	}
 
+	// Leer JSON enviado desde el navegador
 	var datos struct {
 		Texto string `json:"texto"`
 		Morse string `json:"morse"`
@@ -51,12 +55,15 @@ func traducirHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var resultado string
+	// Si los datos que llegaron son texto, convertir a morse
 	if datos.Texto != "" {
 		resultado = morse.TextToMorse(datos.Texto)
+	// Si los datos que llegaron son morse, convertir a texto	
 	} else if datos.Morse != "" {
 		resultado = morse.MorseToText(datos.Morse)
 	}
 
+	// Responder con un JSON
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{
 		"resultado": resultado,
