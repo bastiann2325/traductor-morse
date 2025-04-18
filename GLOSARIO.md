13751ad868ca4db13602d0aacde0e8e1fa69ccfe
Juan Sebastian Mejia Zuniga <juansebas7844@gmail.com>
2025-04-17 20:26:35 -0500

    fix: Corregir lógica de morse.go, eliminar código innecesario

---
+++ GLOSARIO.md
@@ -0,0 +1,10 @@
+# Glosario para el archivo morse.go
+
+- map[]: vector asociativo, es el equivalente a un diccionario o arreglo asociativo en otros lenguajes de programación.
+- range: cláusula utilizada para recorrer colecciones como slices (arreglos).
+- rune:
+- " _ ": el caracter _ significa que es una variable de descarte. Lo verás en los ciclos for de este programa, en esos casos significará que se va ignorar el índice (porque no se necesita).
+- Upper(): funció que se utiliza para convertir una cadena de texto a mayúsculas.
+- Join(): función que se utiliza para combinar elementos de una lista (slice) de cadenas en una sola cadena de texto. 
+- Split(): funcción que se utiliza para dividir una cadena de subcadenas con base en un delimitador específico (el pasado por parámetro).
+- 
--- morse/morse.go
+++ morse/morse.go
@@ -1,20 +1,22 @@
+// Definir el paquete morse
 package morse
 
+// Importar el paquete strings para trabajar con cadenas de texto
 import "strings"
 
-// Almacenar mapa inverso
+// reverseCode almacena un mapa inverso del alfabeto Morse
 var reverseCode map[string]string
 
-// Reconstruir mapa inverso
+// Inicializar mapa inverso usando datos de morseCode
 func init() {
 	reverseCode = make(map[string]string)
-	for letter, codigo := range morseCode {
-		reverseCode[codigo] = string(letter)
+	for letter, code := range morseCode {
+		reverseCode[code] = string(letter)
 	}
 }
 
-// Alfabeto Morse
-var morseCode = map[rune]string {
+// Diccionario Morse
+var morseCode = map[rune]string{
 	'A': ".-",
 	'B': "-...",
 	'C': "-.-.",
@@ -58,42 +60,42 @@ var morseCode = map[rune]string {
 // Función para convertir texto a Morse
 func TextToMorse(text string) string {
 	// Convertir texto a mayúsculas
-    text = strings.ToUpper(text)
-	// Variable para almacenar Morse equivalente a cada letter del texto
-    var result []string
+	text = strings.ToUpper(text)
+	// slice para almacenar Morse equivalente a cada letra
+	var result []string
 	// Recorrer cada caracter del texto
-    for _, char := range text {
+	for _, char := range text {
 		// Buscar valor del caracter en el diccionario
-        if code, ok := morseCode[char]; ok {
-			// Si existe en el diccionario, agregar al código Morse 
-            result = append(result, code)
-			} else {
+		if code, ok := morseCode[char]; ok {
+			// Si existe en el diccionario, agregar al código Morse
+			result = append(result, code)
+		} else {
 			// No existe en el diccionario, reemplazar el caracter por un '?'
-            result = append(result, "?")
-        }
-    }
+			result = append(result, "?")
+		}
+	}
 	// Unir códigos morse almacenados en "result" y separarlos por espacios
-    return strings.Join(result, " ")
+	return strings.Join(result, " ")
 }
 
 // Función para convertir Morse a texto
 func MorseToText(morse string) string {
+	// slice para almacenar letra/número equivalente a cada código Morse 
 	var result []string
-	words := strings.Split(morse, " ")
+	// Dividir cadena de texto Morse para almacenar cada código Morse por separado
+	words := strings.Split(morse, " ") // La divide en partes usando el espacio " " como separador
 
-	// Diccionario inverso
-	reverse := make(map[string]string)
-	for letter, codigo := range morseCode {
-		reverse[codigo] = string(letter)
-	}
-
+	// Recorrer cada código morse en la lisa (slice) words
 	for _, symbol := range words {
+		// Verificar si el morse existe en el mapa
 		if letter, ok := reverseCode[symbol]; ok {
+			// Existe, convertirlo en letra y agregarlo al slice result
 			result = append(result, letter)
 		} else {
+			// No existe, reemplazar el/los caracteres ingresados por "?"
 			result = append(result, "?")
 		}
 	}
-
+	// Unir letras almacenadas en "result" y NO separarlos, por eso el ""
 	return strings.Join(result, "")
 }
